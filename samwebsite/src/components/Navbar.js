03d4afb692922c606645633f8b158af80b849f99
Samantha Whitaker <s.whita6@gmail.com>
2024-08-07 14:16:56 -0500

    Navbar completed

---
+++ samwebsite/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import logo from '../logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light">
+<div class="container">
+
+  <a class="navbar-brand" href="#home"><img className="logo" src={logo} alt="Samantha Whitaker"></img></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <FontAwesomeIcon icon={faBars} style={{color: "black"}}/>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Me</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Portfolio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
+  )
+}
+
+export default Navbar
