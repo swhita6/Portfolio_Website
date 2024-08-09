746cc3ca32d35b38cf7bca6ebc5d88349fc69dca
Samantha Whitaker <s.whita6@gmail.com>
2024-08-09 11:12:37 -0500

    Added Header and Cards

--- samwebsite/src/App.js
+++ samwebsite/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Cards from './components/Cards';
 
 function App() {
-  return (
+    return ( 
+    <>
     <Navbar />
-  );
-}
+    <Header />
+    <Cards />
+    </>
+    )
+};
 
 export default App;
