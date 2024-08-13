c71ee4358c8e386ead111806b35894049880c1f0
Samantha Whitaker <s.whita6@gmail.com>
2024-08-13 14:05:41 -0500

    Elements removed from Header

--- samwebsite/src/components/Header.js
+++ samwebsite/src/components/Header.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import bitmoji from '../bitmoji_no_bg.png'
 
 const Header = () => {
   return (
-    <div className='header-wrapper'>
-        <div className='main-img'>
-            <img id='avatar-img' src={bitmoji} alt='avatar head'></img>
-        </div>
+    <div id='home' className='header-wrapper'>
         <div className='main-info'>
             <h1>Hi, I'm Sam!</h1>
         </div>  
